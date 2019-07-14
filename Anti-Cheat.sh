a6e85f3d3624583ded7d233f373d7027aeeb5c94
SH4FS0c13ty <52138924+SH4FS0c13ty@users.noreply.github.com>
2019-07-14 22:16:12 +0200

    Update Anti-Cheat.sh

--- Anti-Cheat.sh
+++ Anti-Cheat.sh
@@ -1,5 +1,6 @@
 #! /bin/sh -
 
 echo -n -e "\033]0;Anti-Cheat Discord Bot\007"
+clear
 echo "Starting Anti-Cheat Discord Bot ..."
 python main.py <OAUTH2_CLIENT_ID> <OAUTH2_CLIENT_SECRET> <CLIENT_ID>
